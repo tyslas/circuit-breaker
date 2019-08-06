2d8d4826542d8f3913641d34ddc5e387db4f2aa9
Tito Yslas <roberto.yslas@gmail.com>
2019-08-05 18:27:09 -0600

    implemented circuit breaker pattern via hystrix

--- reading/src/main/java/hello/ReadingApplication.java
+++ reading/src/main/java/hello/ReadingApplication.java
@@ -1,22 +1,31 @@
 package hello;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.client.RestTemplate;
-import java.net.URI;
 
 @RestController
 @SpringBootApplication
+@EnableCircuitBreaker
 public class ReadingApplication {
 
-    @RequestMapping("/to-read")
-    public String readingList() {
-        RestTemplate restTemplate = new RestTemplate();
-        URI uri = URI.create("http://localhost:8090/recommended");
+    @Autowired
+    private BookService bookService;
 
-        return restTemplate.getForObject(uri, String.class);
+    @Bean
+    public RestTemplate rest(RestTemplateBuilder builder) {
+        return builder.build();
+    }
+
+    @RequestMapping("/to-read")
+    public String toRead() {
+        return bookService.readingList();
     }
 
     public static void main(String[] args) {
